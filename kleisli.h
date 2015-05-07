43519a020edc87c5da5c0baa831e3901eafeb04a
Kevin Dungs <kevin@dun.gs>
2015-05-07 18:00:20 +0200

    Add documentation specify that bind is only implemented for optional<T> because it requires the !- and the *-operator.

--- kleisli.h
+++ kleisli.h
@@ -7,10 +7,12 @@
 #include <experimental/optional>
 using std::experimental::optional;
 
-// (>>=) :: m a -> (a -> m b) -> m b
-// Bind     M<A>   FN........    M<B>
-template <template <typename> class M, typename A, typename FN,
-          typename B = typename std::result_of_t<FN(A)>::value_type>
+/** Implement monadic bind (>>=) for std::experimental::optional.
+ * (>>=) :: m a -> (a -> m b) -> m b
+ */
+template <typename A, typename FN,
+          typename B = typename std::result_of_t<FN(A)>::value_type,
+          template <typename> class M = optional>
 auto Bind(M<A> a, const FN& f) {
   if (!a) {
     return M<B>{};
@@ -18,8 +20,9 @@ auto Bind(M<A> a, const FN& f) {
   return f(*a);
 }
 
-// (>=>) :: (a -> m b) -> (b -> m c) -> a -> m c
-// Kleisli  FN_LHS....    FN_RHS....    A    M<C>
+/** Implement Kleisli composition (>=>) independently of the monad in question.
+ * (>=>) :: (a -> m b) -> (b -> m c) -> a -> m c
+ */
 template <typename FN_LHS, typename FN_RHS>
 auto Kleisli(const FN_LHS& fl, const FN_RHS& fr) {
   return [&fl, &fr](const auto& a) { return Bind(fl(a), fr); };
